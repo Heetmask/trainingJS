bfb71d7207cb3c929882ba9f478289bc9a9b416f
Heetmask <nikitanovdm@gmail.com>
2025-09-10 22:16:07 +0300

    learn promises and convert text into promises

--- data/cart.js
+++ data/cart.js
@@ -113,3 +113,14 @@ matchingItem.deliveryOptionId = deliveryOptionId;
 
 saveToStorage();
 }
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+ console.log(xhr.response);
+ fun();
+  });
+  xhr.open('GET','https://supersimplebackend.dev/cart');
+  xhr.send();
+}
